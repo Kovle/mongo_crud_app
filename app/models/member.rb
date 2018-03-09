25c39252ae27fc72193e115b3a573ee8551fe688
Angel Mendez <angelmendezz751@gmail.com>
2018-03-09 13:43:34 -0600

    Conclusión, al leer 1000 registros en ambas BD, mongo resultó 4 veces más rápido, esto no quiere decir que sea aplicable a todos los proyectos, cada uno tiene diferente estructura y cosas que resolver muy diferentes

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,18 +1,19 @@
 class Member
   include Mongoid::Document
-  
   after_create :create_more_members
   field :name, type: String
   field :age, type: Integer
 
   private
     def random_string
-      o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
-      string = (0...50).map { o[rand(o.length)] }.join
+
     end
     def create_more_members
-      100.times do |x|
-        Member.create!(name: "Angel") do |doc|
+      900.times do |x|
+        puts "Member ##{x}"
+        o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+        string = (0...50).map { o[rand(o.length)] }.join
+        Member.create!(name: string) do |doc|
           doc.age = 12
         end
       end
